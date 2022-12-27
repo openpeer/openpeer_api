5a887ef2b27be34f7d460e7d1bce4343b22e32d3
Marcos Teixeira <m.viniteixeira@gmail.com>
2022-12-27 17:28:50 +0000

    Fix relation

--- app/models/payment_method.rb
+++ app/models/payment_method.rb
@@ -1,7 +1,8 @@
 class PaymentMethod < ApplicationRecord
-  belongs_to :user, :bank
+  belongs_to :user
+  has_and_belongs_to_many :banks
 
   def as_json(options)
-    super({ include: [:user, :bank] }.merge(options))
+    super({ include: [:user, :banks] }.merge(options))
   end
 end
