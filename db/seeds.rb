8737f81974f4374793a93c896cc66f9f6efcde95
Marcos Teixeira <m.viniteixeira@gmail.com>
2023-03-09 11:36:45 +0000

    Improve seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@
 
 
 currencies = FiatCurrency.create([
+  { name: 'Indian Ruppee', code: 'INR', symbol: '₹', country_code: 'IN' },
   { name: 'Brazilian Real', code: 'BRL', symbol: 'R$', country_code: 'BR' },
   { name: 'Australian Dollar', code: 'AUD', symbol: 'A$', country_code: 'AU' },
-  { name: 'Indian Ruppee', code: 'INR', symbol: '₹', country_code: 'IN' },
   { name: 'US Dollar', code: 'USD', symbol: '$', country_code: 'US' },
   { name: 'Singapore Dollar', code: 'SGD', symbol: '$', country_code: 'SG' },
   { name: 'Hong Kong Dollar', code: 'HKD', symbol: '$', country_code: 'HK' },
@@ -18,9 +18,9 @@ currencies = FiatCurrency.create([
 ])
 
 Bank.create([
-  { name: "PIX", fiat_currency: currencies[0], account_info_schema: [{"label"=>"Pix Key", "id"=>"pix_key", "required"=>true }, { "label"=>"Details", "id"=>"details", "type"=>"textarea", "required"=>false }] },
+  { name: "UPI", fiat_currency: currencies[0], account_info_schema: [{"label"=>"UPI ID", "id"=>"upi_id", "required"=>true }, { "label"=>"Details", "id"=>"details", "type"=>"textarea", "required"=>false }] },
   { name: "PayID", fiat_currency: currencies[1], account_info_schema: [{"label"=>"PayID Phone or Email", "id"=>"payid", "required"=>true }, { "label"=>"Details", "id"=>"details", "type"=>"textarea", "required"=>false }] },
-  { name: "UPI", fiat_currency: currencies[2], account_info_schema: [{"label"=>"UPI ID", "id"=>"upi_id", "required"=>true }, { "label"=>"Details", "id"=>"details", "type"=>"textarea", "required"=>false }] },
+  { name: "PIX", fiat_currency: currencies[2], account_info_schema: [{"label"=>"Pix Key", "id"=>"pix_key", "required"=>true }, { "label"=>"Details", "id"=>"details", "type"=>"textarea", "required"=>false }] },
   { name: 'Revolut', account_info_schema: [{"label"=>"Revtag", "id"=>"revtag", "required"=>true }, { "label"=>"Details", "id"=>"details", "type"=>"textarea", "required"=>false }]},
   { name: 'Bank Transfer', account_info_schema: [{"label"=>"Account Name", "id"=>"account_name", "required"=>true }, {"label"=>"Account Number", "id"=>"account_number", "required"=>true },
                                                  { "label"=>"Details", "id"=>"details", "type"=>"textarea", "required"=> false },
@@ -30,4 +30,11 @@ Bank.create([
 Token.create([{ chain_id: 80001, address: "0x0000000000000000000000000000000000000000",
                 decimals: 18, name: 'Matic MUMBAI', symbol: 'MATIC', coingecko_id: 'matic-network' },
               { chain_id: 80001, address: "0x04B2A6E51272c82932ecaB31A5Ab5aC32AE168C3",
-                decimals: 18, name: 'GFARMDAI', symbol: 'GFARMDAI', coingecko_id: 'dai' }])
+                decimals: 18, name: 'GFARMDAI', symbol: 'GFARMDAI', coingecko_id: 'dai' },
+              { address: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F", decimals: 6,
+                   symbol: "USDT", name: "USDT", chain_id: 137, coingecko_id: "tether" },
+              { address: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
+                 decimals: 6, symbol: "USDC", name: "USDC", chain_id: 137, coingecko_id: "usd-coin" },
+              { address: "0x0000000000000000000000000000000000000000", decimals: 18, symbol: "MATIC",
+                 name: "MATIC", chain_id: 137, coingecko_id: "matic-network" }
+              ])
