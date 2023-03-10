d8119446053b6ba7d21aa07f1b9bb738fedd7498
Marcos Teixeira <m.viniteixeira@gmail.com>
2023-03-10 14:10:27 +0000

    Flags and countries

--- app/serializers/fiat_currency_serializer.rb
+++ app/serializers/fiat_currency_serializer.rb
@@ -2,6 +2,6 @@ class FiatCurrencySerializer < ActiveModel::Serializer
   attributes :id, :code, :name, :country_code, :symbol
 
   attribute :icon do
-    "https://raw.githubusercontent.com/hampusborgos/country-flags/main/png250px/#{object.country_code.downcase}.png"
+    object.country_code
   end
 end
