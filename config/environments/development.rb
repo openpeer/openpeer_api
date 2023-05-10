77799065d861bdf7f399502f873044453e1de847
Marcos Teixeira <m.viniteixeira@gmail.com>
2023-05-10 14:21:08 +0100

    Development config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,7 +61,8 @@ Rails.application.configure do
   # config.action_view.annotate_rendered_view_with_filenames = true
 
   # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
+  config.action_cable.disable_request_forgery_protection = true
 
   config.hosts << /[a-z0-9\-\.]+\.ngrok\.io/
+  config.hosts << /[a-z0-9\-\.]+\.ngrok-free\.app/
 end
