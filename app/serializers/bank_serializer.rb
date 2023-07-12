f48523c9427ffbb8fa2264d671e32a7ec88e67e7
Marcos Teixeira <m.viniteixeira@gmail.com>
2023-07-12 15:21:54 +0100

    Use the uploaded image in the app

--- app/serializers/bank_serializer.rb
+++ app/serializers/bank_serializer.rb
@@ -2,10 +2,6 @@ class BankSerializer < ActiveModel::Serializer
   attributes :id, :name, :account_info_schema
 
   attribute :icon do
-    "/payment_channel/#{object.name.downcase}.svg"
-  end
-
-  attribute :image do
     object.image.url if object.image.attached?
   end
 end
