767a2f0a4c25fd0ae1b95fd6390cfa8979ef0a3a
Marcos Teixeira <m.viniteixeira@gmail.com>
2023-07-28 12:50:57 +0100

    Fix development images

--- app/serializers/bank_serializer.rb
+++ app/serializers/bank_serializer.rb
@@ -2,6 +2,18 @@ class BankSerializer < ActiveModel::Serializer
   attributes :id, :name, :account_info_schema
 
   attribute :icon do
-    object.image.url if object.image.attached?
+    if object.image.attached?
+      if Rails.env.development?
+        Rails.application.routes.url_for(
+          controller: 'active_storage/blobs/redirect',
+          action: :show,
+          signed_id: object.image.signed_id,
+          filename: object.image.filename.to_s,
+          host: 'localhost:5000'
+        )
+      else
+        object.image.url
+      end
+    end
   end
 end
