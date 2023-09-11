b866d7214cf27d18798192d3ad27a58d018b671e
Marcos Teixeira <m.viniteixeira@gmail.com>
2023-09-11 17:46:07 +0100

    Add chains explorer addressess

--- app/models/escrow.rb
+++ app/models/escrow.rb
@@ -14,7 +14,12 @@ class Escrow < ApplicationRecord
   def explorers
     {
       137 => 'https://polygonscan.com',
-      80001 => 'https://mumbai.polygonscan.com'
+      80001 => 'https://mumbai.polygonscan.com',
+      56 => 'https://bscscan.com',
+      10 => 'https://optimistic.etherscan.io',
+      42161 => 'https://arbiscan.io',
+      43114 => 'https://cchain.explorer.avax.network',
+      1 => 'https://etherscan.io'
     }
   end
 end
