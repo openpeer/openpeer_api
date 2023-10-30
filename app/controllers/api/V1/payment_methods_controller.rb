485a0112224bf92e88be5088eacf0f750b2a2adb
Marcos Teixeira <m.viniteixeira@gmail.com>
2023-10-30 12:56:45 +0000

    Update payment methods to fetch only the connected user

--- app/controllers/api/V1/payment_methods_controller.rb
+++ app/controllers/api/V1/payment_methods_controller.rb
@@ -1,10 +1,10 @@
 module Api
   module V1
-    class PaymentMethodsController < BaseController
+    class PaymentMethodsController < JwtController
       def index
-        @payment_methods = ListPaymentMethod.joins(:user).left_joins(bank: :fiat_currencies)
-                                            .where(banks_fiat_currencies: { fiat_currency_id: [params[:currency_id], nil]})
-                                            .where('lower(users.address) = ?', params[:address].downcase)
+        @payment_methods = current_user.list_payment_methods.
+                                       .includes(:user).left_joins(bank: :fiat_currencies)
+                                       .where(banks_fiat_currencies: { fiat_currency_id: [params[:currency_id], nil]})
         render json: @payment_methods, each_serializer: PaymentMethodSerializer, status: :ok, root: 'data'
       end
     end
