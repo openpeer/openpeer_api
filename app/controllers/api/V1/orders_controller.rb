cc1438493683ab6be616a984b03c518dde1fa7f1
Marcos Teixeira <m.viniteixeira@gmail.com>
2023-11-01 09:20:57 +0000

    Save orders with a payment method

--- app/controllers/api/V1/orders_controller.rb
+++ app/controllers/api/V1/orders_controller.rb
@@ -9,44 +9,34 @@ module Api
       end
 
       def create
-        if JSON.parse(params[:order].to_json) == JSON.parse(params[:message])
-          if (Eth::Signature.verify(params[:message], params[:data], params[:address]) rescue false)
-            @order = Order.new(order_params)
+        @order = Order.new(order_params)
 
-            if @order.list.accept_only_verified? && !current_user.verified?
-              return render json: { data: { message: 'Order not created' }}, status: :ok
-            end
+        if @order.list.accept_only_verified? && !current_user.verified?
+          return render json: { data: { message: 'Order not created' }}, status: :ok
+        end
 
-            if @order.list.buy_list?
-              @order.seller = current_user
-              @order.buyer = @order.list.seller
-            else
-              @order.seller = @order.list.seller
-              @order.buyer = current_user
-            end
+        if @order.list.buy_list?
+          @order.seller = current_user
+          @order.buyer = @order.list.seller
+        else
+          @order.seller = @order.list.seller
+          @order.buyer = current_user
+        end
 
-            Order.transaction do
-              if @order.list.buy_list?
-                @order.payment_method = create_or_update_payment_method
-              else
-                order_payment_method = @order.list.payment_method.dup
-                order_payment_method = order_payment_method.becomes!(OrderPaymentMethod)
-                @order.payment_method = order_payment_method
-              end
-              @order.chain_id = @order.list.chain_id
-              @order.deposit_time_limit = @order.list.deposit_time_limit
-              @order.payment_time_limit = @order.list.payment_time_limit
-              if @order.save
-                NotificationWorker.perform_async(NotificationWorker::NEW_ORDER, @order.id)
-                if @order.deposit_time_limit.to_i > 0
-                  AutomaticCancellationWorker.perform_in(@order.deposit_time_limit.minutes,
-                    @order.id, AutomaticCancellationWorker::DEPOSIT)
-                end
-                render json: @order, serializer: OrderSerializer, status: :ok, root: 'data'
-              else
-                render json: { data: { message: 'Order not created', errors: @order.errors }}, status: :ok
-              end
+        Order.transaction do
+          @order.payment_method = create_or_update_payment_method
+          @order.chain_id = @order.list.chain_id
+          @order.deposit_time_limit = @order.list.deposit_time_limit
+          @order.payment_time_limit = @order.list.payment_time_limit
+          if @order.save
+            NotificationWorker.perform_async(NotificationWorker::NEW_ORDER, @order.id)
+            if @order.deposit_time_limit.to_i > 0
+              AutomaticCancellationWorker.perform_in(@order.deposit_time_limit.minutes,
+                @order.id, AutomaticCancellationWorker::DEPOSIT)
             end
+            render json: @order, serializer: OrderSerializer, status: :ok, root: 'data'
+          else
+            render json: { data: { message: 'Order not created', errors: @order.errors }}, status: :ok
           end
         end
       end
@@ -88,25 +78,26 @@ module Api
       end
 
 
-      def payment_method_params
+      def copy_payment_method_params
         params.require(:order)
-              .require(:payment_method).permit(:id, values: {})
+              .require(:payment_method).permit(:id, :bank_id, values: {})
+      end
+
+      def new_payment_method_params
+        params.require(:order)
+              .require(:payment_method).permit(:bank_id, values: {})
       end
 
       def create_or_update_payment_method
-        if payment_method_params[:id]
-          @payment_method = ListPaymentMethod.find(payment_method_params[:id])
-          if (@payment_method.user == current_user)
-            @payment_method.update(payment_method_params)
-            order_payment_method = @payment_method.dup
-            order_payment_method = order_payment_method.becomes!(OrderPaymentMethod)
-            order_payment_method.save
-            @payment_method = order_payment_method
-          end
-        else
-          @payment_method = OrderPaymentMethod.new(payment_method_params)
+        if copy_payment_method_params[:id]
+          @payment_method = ListPaymentMethod.find(copy_payment_method_params[:id])
+          order_payment_method = @payment_method.dup
+          order_payment_method = order_payment_method.becomes!(OrderPaymentMethod)
+          order_payment_method.save
+          @payment_method = order_payment_method
+        elsif new_payment_method_params[:bank_id]
+          @payment_method = OrderPaymentMethod.new(new_payment_method_params)
           @payment_method.user = current_user
-          @payment_method.bank_id = @order.list.bank_id
           @payment_method.save
         end
         @payment_method
