59af6ee81d3a5c3e2074e4b8512c7baa37e01eb3
Marcos Teixeira <m.viniteixeira@gmail.com>
2023-11-03 17:15:38 +0000

    Enable worker content

--- app/workers/price_discovery/coordinator_worker.rb
+++ app/workers/price_discovery/coordinator_worker.rb
@@ -5,13 +5,12 @@ module PriceDiscovery
     include Sidekiq::Worker
 
     def perform
-      puts "Calculating price discovery for all tokens and fiat currencies..."
-      # Token.where(allow_binance_rates: true).pluck(:symbol).uniq.each do |token|
-      #   FiatCurrency.where(allow_binance_rates: true).pluck(:code).uniq.each do |fiat|
-      #     PriceDiscovery::FetchWorker.perform_async(token, fiat, 'BUY')
-      #     PriceDiscovery::FetchWorker.perform_async(token, fiat, 'SELL')
-      #   end
-      # end
+      Token.where(allow_binance_rates: true).pluck(:symbol).uniq.each do |token|
+        FiatCurrency.where(allow_binance_rates: true).pluck(:code).uniq.each do |fiat|
+          PriceDiscovery::FetchWorker.perform_async(token, fiat, 'BUY')
+          PriceDiscovery::FetchWorker.perform_async(token, fiat, 'SELL')
+        end
+      end
     end
   end
 end
