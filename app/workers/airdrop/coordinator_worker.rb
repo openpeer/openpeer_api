38dd9e54de9359ce140e9b3850e1249cf563eaab
Marcos Teixeira <m.viniteixeira@gmail.com>
2023-12-04 15:09:48 +0000

    Fix worker

--- app/workers/airdrop/coordinator_worker.rb
+++ app/workers/airdrop/coordinator_worker.rb
@@ -5,7 +5,7 @@ module Airdrop
     include Sidekiq::Worker
 
     def perform
-      Contract.where('version::integer = ?',  Setting['contract_version']).pluck(:id) do |contract_id|
+      Contract.where('version::integer = ?', Setting['contract_version']).pluck(:id).each do |contract_id|
         Airdrop::PointsWorker.perform_async(contract_id, Time.now.utc.to_i)
       end
     end
