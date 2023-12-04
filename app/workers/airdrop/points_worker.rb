6a8d9b6e5091d24b326c82421fffdf771fb4b941
Marcos Teixeira <m.viniteixeira@gmail.com>
2023-12-04 12:43:48 +0000

    Add points to the contracts

---
+++ app/workers/airdrop/points_worker.rb
@@ -0,0 +1,31 @@
+require 'eth'
+
+module Airdrop
+  class PointsWorker
+    include Sidekiq::Worker
+    attr_accessor :contract
+    POINTS_PER_USD = 0.000832
+
+    def perform(contract_id, time = Time.now.utc.to_i)
+      @contract = Contract.find(contract_id)
+      
+      Token.where(chain_id: contract.chain_id).find_each do |token|
+        token_contract = Eth::Contract.from_abi(abi: abi, address: token.address, name: token.name)
+        balance = client.call(token_contract, 'balanceOf', contract.address)
+        usd_value = (balance.to_f / 10 ** token.decimals) * token.price_in_currency('USD')
+        points = (POINTS_PER_USD * usd_value)
+        contract.update(points: (contract.points || 0) + points)
+      end
+    end
+
+    private
+
+    def abi
+      @abi ||= File.read(Rails.root.join('config', 'abis', 'erc20.json'))
+    end
+
+    def client
+      @client ||= Eth::Client.create(ENV["NODE_URL_#{contract.chain_id}"])
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_30_124828) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_04_124256) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -107,6 +107,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_30_124828) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "version"
+    t.decimal "points"
     t.index ["user_id", "chain_id", "address", "version"], name: "index_contracts_on_user_id_and_chain_id_and_address_and_version", unique: true
     t.index ["user_id"], name: "index_contracts_on_user_id"
   end
