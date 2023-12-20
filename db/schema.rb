8c1492560d66ac3ac6462b96c4ead3ef7bdf3d7f
Marcos Teixeira <m.viniteixeira@gmail.com>
2023-12-20 12:06:37 +0000

    Add imported orders count to the users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_04_161007) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_20_120428) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -302,6 +302,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_04_161007) do
     t.integer "available_from"
     t.integer "available_to"
     t.boolean "weekend_offline", default: false
+    t.integer "imported_orders_count", default: 0
     t.index "lower((address)::text)", name: "index_users_on_lower_address", unique: true
     t.index ["merchant"], name: "index_users_on_merchant"
     t.index ["name"], name: "index_users_on_name", unique: true
