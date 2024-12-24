cefdc81893e56aad1adc38c4a2dfbb1680235e29
George Donnelly <me@georgedonnelly.com>
2024-12-24 08:01:08 -0500

    remove walletchat

--- app/workers/update_user_worker.rb
+++ app/workers/update_user_worker.rb
@@ -6,22 +6,5 @@ class UpdateUserWorker
   def perform(user_id)
     user = User.find(user_id)
     Knock::Users.identify(id: user.address, data: { name: user.name, email: user.email })
-    
-    return unless ENV['WALLET_CHAT_API_KEY'].present?
-
-    url = 'https://api.v2.walletchat.fun/v1/name'
-    headers = {
-      'Content-Type' => 'application/json',
-      'Authorization' => "Bearer #{ENV['WALLET_CHAT_API_KEY']}"
-    }
-    payload = {
-      address: user.address,
-      email: user.email,
-      name:  user.name,
-      signupsite:  'app.openpeer.xyz',
-      domain: 'openpeer.xyz'
-    }.to_json
-    
-    RestClient.post(url, payload, headers)
   end
 end
